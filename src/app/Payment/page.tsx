e0f9fcc0907c2e227706cd2562b64a603382ef59
KaritSung <113464985+KaritSung@users.noreply.github.com>
2024-04-26 01:28:28 +0700

    fix key props

--- src/app/Payment/page.tsx
+++ src/app/Payment/page.tsx
@@ -6,130 +6,206 @@ import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 const StyledBox = styled(Box)(({ theme }) => ({
-    background: "#fff",
-    cursor: 'pointer',
-    borderRadius: '12px',
+  background: "#fff",
+  cursor: "pointer",
+  borderRadius: "12px",
 
-    '.arrow_right': {
-        display: "none"
-    },
-    '&:hover': {
-        border: '1px solid #2D3DDF',
-        boxShadow: 5
-    },
-    '&:hover .arrow_right': {
-        display: "block"
-    }
+  ".arrow_right": {
+    display: "none",
+  },
+  "&:hover": {
+    border: "1px solid #2D3DDF",
+    boxShadow: 5,
+  },
+  "&:hover .arrow_right": {
+    display: "block",
+  },
 }));
 
 export default function Payment() {
-    const router = useRouter();
-    const pathname = usePathname();
-    const [paymentChannels, setPaymentChannels] = useState<PaymentMethod[]>();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [paymentChannels, setPaymentChannels] = useState<PaymentMethod[]>();
 
-    const fetchPaymentChannels = async () => {
-        try {
-            const response = await fetch('/mocks/payment_channels.json');
-            const data = await response.json();
-            console.log('fetching PaymentChannels', data);
-            setPaymentChannels(data);
-        } catch (error) {
-            console.error('Error fetching PaymentChannels:', error);
-        }
-    };
+  const fetchPaymentChannels = async () => {
+    try {
+      const response = await fetch("/mocks/payment_channels.json");
+      const data = await response.json();
+      console.log("fetching PaymentChannels", data);
+      setPaymentChannels(data);
+    } catch (error) {
+      console.error("Error fetching PaymentChannels:", error);
+    }
+  };
 
-    useEffect(() => {
-        fetchPaymentChannels();
-    }, [])
+  useEffect(() => {
+    fetchPaymentChannels();
+  }, []);
 
-    return (
-        <Stack direction={'row'}>
-            <Box display={'table-cell'} sx={{ width: '60%', height: 'auto', p: 2 }} justifyContent={'center'}>
-                <Box mx={3} my={10}>
-                    <Typography fontSize={'24px'} fontWeight={'400'} color={'#221F3C'}>Payment Details</Typography>
-                    <Stack direction={'column'} spacing={2} mt={3} ml={3}>
-                        {paymentChannels?.map((item) => {
-                            return (
-                                <StyledBox width={'50%'} onClick={() => {
-                                    router.replace(pathname + '/Complete')
-                                }}>
-                                    <Stack direction={'row'} justifyContent={'space-between'}>
-                                        <Box py={2} px={2} display={'flex'} flexDirection={'row'} alignContent={'center'}>
-                                            <img src={item.payment_img} alt="Logo"></img>
-                                            <Box ml={2} display={'flex'} alignItems={'center'} justifyContent={'center'}>
-                                                <Typography fontSize={'20px'} fontWeight={'400'} color={'#221F3C'}>{item.payment_name}</Typography>
-                                            </Box>
-                                        </Box>
-                                        <Box display={'flex'} alignItems={'center'} justifyContent={'end'}><ChevronRight color="#2D3DDF" className="arrow_right" /></Box>
-                                    </Stack>
-
-                                </StyledBox>
-                            )
-                        })}
-
-                    </Stack>
-
-
-                </Box>
-            </Box>
-            <Box display={'table-cell'} sx={{ width: '40%' }}>
-                <Box display={'flex'} width={'100%'} justifyContent={'center'}>
-                    <Paper sx={{ width: '55%', mt: 16 }} >
-                        <Grid py={3} px={2} container justifyContent={'center'} rowSpacing={1} width={'100%'}>
-                            <Grid item md={6}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#737373'}>
-                                    {'Base fare'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} textAlign={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#B7BCF3'}>
-                                    {'1,000.00'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} justifyContent={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#737373'}>
-                                    {'Total discount'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} textAlign={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#B7BCF3'}>
-                                    {'0.00'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} justifyContent={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#737373'}>
-                                    {'Price after discount'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} textAlign={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#B7BCF3'}>
-                                    {'1,000.00'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} justifyContent={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#737373'}>
-                                    {'Taxes & service fees'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} textAlign={'end'}>
-                                <Typography fontSize={'16px'} fontWeight={'300'} color={'#B7BCF3'}>
-                                    {'140.00'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} justifyContent={'end'}>
-                                <Typography fontSize={'22px'} fontWeight={'500'} color={'#221F3C'}>
-                                    {'Total Amount'}
-                                </Typography>
-                            </Grid>
-                            <Grid item md={6} textAlign={'end'}>
-                                <Typography fontSize={'22px'} fontWeight={'500'} color={'#2D3DDF'}>
-                                    {'1,140.00'}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Paper>
-                </Box>
-            </Box>
-        </Stack>
-    );
+  return (
+    <Stack direction={"row"}>
+      <Box
+        display={"table-cell"}
+        sx={{ width: "60%", height: "auto", p: 2 }}
+        justifyContent={"center"}
+      >
+        <Box mx={3} my={10}>
+          <Typography fontSize={"24px"} fontWeight={"400"} color={"#221F3C"}>
+            Payment Details
+          </Typography>
+          <Stack direction={"column"} spacing={2} mt={3} ml={3}>
+            {paymentChannels?.map((item) => {
+              return (
+                <StyledBox
+                  key={item.id}
+                  width={"50%"}
+                  onClick={() => {
+                    router.replace(pathname + "/Complete");
+                  }}
+                >
+                  <Stack direction={"row"} justifyContent={"space-between"}>
+                    <Box
+                      py={2}
+                      px={2}
+                      display={"flex"}
+                      flexDirection={"row"}
+                      alignContent={"center"}
+                    >
+                      <img src={item.payment_img} alt="Logo"></img>
+                      <Box
+                        ml={2}
+                        display={"flex"}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                      >
+                        <Typography
+                          fontSize={"20px"}
+                          fontWeight={"400"}
+                          color={"#221F3C"}
+                        >
+                          {item.payment_name}
+                        </Typography>
+                      </Box>
+                    </Box>
+                    <Box
+                      display={"flex"}
+                      alignItems={"center"}
+                      justifyContent={"end"}
+                    >
+                      <ChevronRight color="#2D3DDF" className="arrow_right" />
+                    </Box>
+                  </Stack>
+                </StyledBox>
+              );
+            })}
+          </Stack>
+        </Box>
+      </Box>
+      <Box display={"table-cell"} sx={{ width: "40%" }}>
+        <Box display={"flex"} width={"100%"} justifyContent={"center"}>
+          <Paper sx={{ width: "55%", mt: 16 }}>
+            <Grid
+              py={3}
+              px={2}
+              container
+              justifyContent={"center"}
+              rowSpacing={1}
+              width={"100%"}
+            >
+              <Grid item md={6}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#737373"}
+                >
+                  {"Base fare"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} textAlign={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#B7BCF3"}
+                >
+                  {"1,000.00"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} justifyContent={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#737373"}
+                >
+                  {"Total discount"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} textAlign={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#B7BCF3"}
+                >
+                  {"0.00"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} justifyContent={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#737373"}
+                >
+                  {"Price after discount"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} textAlign={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#B7BCF3"}
+                >
+                  {"1,000.00"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} justifyContent={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#737373"}
+                >
+                  {"Taxes & service fees"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} textAlign={"end"}>
+                <Typography
+                  fontSize={"16px"}
+                  fontWeight={"300"}
+                  color={"#B7BCF3"}
+                >
+                  {"140.00"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} justifyContent={"end"}>
+                <Typography
+                  fontSize={"22px"}
+                  fontWeight={"500"}
+                  color={"#221F3C"}
+                >
+                  {"Total Amount"}
+                </Typography>
+              </Grid>
+              <Grid item md={6} textAlign={"end"}>
+                <Typography
+                  fontSize={"22px"}
+                  fontWeight={"500"}
+                  color={"#2D3DDF"}
+                >
+                  {"1,140.00"}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Box>
+      </Box>
+    </Stack>
+  );
 }
--- src/app/component/sidebar/item.tsx
+++ src/app/component/sidebar/item.tsx
@@ -31,10 +31,8 @@ const SidebarItem = ({ item }: { item: ISidebarItem }) => {
   };
 
   const isActive = useMemo(() => {
-    console.log('alwwwad', pathname.includes('Explore'));
-
     if (items && items.length > 0) {
-      if ((items.find((item) => item.path === pathname))) {
+      if (items.find((item) => item.path === pathname)) {
         setExpanded(true);
         return true;
       }
@@ -43,28 +41,27 @@ const SidebarItem = ({ item }: { item: ISidebarItem }) => {
     return path === pathname;
   }, [items, path, pathname]);
 
-
   return (
     <>
       <div
         className={`w-20 h-20 p-5 rounded-full hover:bg-sidebar-background cursor-pointer drop-shadow-xl 
-        ${isActive || (pathname.includes('Explore') && item.path === '/Explore') || (pathname.includes('Payment') && item.path === '/Explore')
+        ${
+          isActive ||
+          (pathname.includes("Explore") && item.path === "/Explore") ||
+          (pathname.includes("Payment") && item.path === "/Explore")
             ? "text-[#2C3CDE] bg-sidebar-background" // Change "text-red-500" to your desired red color
             : "text-white hover:text-sidebar-active text-sidebar-iconColor hover:text-sidebar-active"
-          }`}
+        }`}
         onClick={onClick}
       >
-
-
         <div>
-          <Stack direction={'column'} alignItems={'center'} spacing={1}>
+          <Stack direction={"column"} alignItems={"center"} spacing={1}>
             <Icon size={20} />
             <p className="text-sm font-semibold">{name} </p>
           </Stack>
         </div>
         {items && items.length > 0 && <ChevronDown size={18} />}
       </div>
-
     </>
   );
 };
